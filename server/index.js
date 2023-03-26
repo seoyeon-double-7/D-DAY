6206319cdc2d7b94238a8bf9a75cff444fe85dad
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2023-03-27 00:25:47 +0900

    ⚡️ : 몽고DB 연결성공

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require("mongoose");
+const config = require("./config/key");
+
+mongoose
+  .connect(config.mongoURI, {})
+  .then(() => console.log("MongoDB Connected.."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
