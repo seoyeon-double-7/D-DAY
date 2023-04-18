8158c49a6f61d6858f09ce3e0553caad22b8a90d
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-04-18 12:19:54 +0900

    ui 구성 완료

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,12 +1,26 @@
+import Footer from "../../Layout/Footer";
+import Nav from "../../Layout/Nav";
 import "../../styles/HomePage.css";
 
 function HomePage() {
+
   return (
     <div className="home">
-      <button className="home-item">MORNING</button>
-      <button className="home-item">AFTERNOON</button>
-      <button className="home-item">DINNER</button>
-      <button className="home-item">NIGHT</button>
+      <Nav/>
+     
+
+      <div className="map-morning"></div>
+      <div className="map-afternonn"></div>
+      <div className="map-dinner"></div>
+      <div className="map-night"></div>
+     
+      {/* <div className="map-afternonn"><Map name="afternonn"/></div>
+      <div className="map-dinner"><Map name="dinner"/></div>
+      <div className="map-night"><Map name="night"/></div> */}
+     
+     <Footer/>
+      <div className="opening"></div>
+      <div className="ending"></div>
     </div>
   );
 }
