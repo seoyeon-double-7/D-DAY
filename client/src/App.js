2856bf6ce4ab7bb41c87f044cc6e1f818b7c5c93
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-04-18 12:20:05 +0900

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Nav/>
+      {/* <Nav/> */}
       
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
