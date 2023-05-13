9529817947d70230dee4c84ba76a881f6454a5f6
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-05-13 13:07:26 +0900

    중복된 내용 삭제

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -3,24 +3,19 @@ import Nav from "../../Layout/Nav";
 import "../../styles/HomePage.css";
 
 function HomePage() {
-
+ 
   return (
     <div className="home">
       <Nav/>
      
 
-      <div className="map-morning"></div>
-      <div className="map-afternonn"></div>
-      <div className="map-dinner"></div>
-      <div className="map-night"></div>
-     
-      {/* <div className="map-afternonn"><Map name="afternonn"/></div>
-      <div className="map-dinner"><Map name="dinner"/></div>
-      <div className="map-night"><Map name="night"/></div> */}
-     
+      <div className="map map-morning"></div>
+      <div className="map map-afternonn"></div>
+      <div className="map map-dinner"></div>
+      <div className="map map-night"></div>
+
      <Footer/>
-      <div className="opening"></div>
-      <div className="ending"></div>
+   
     </div>
   );
 }
