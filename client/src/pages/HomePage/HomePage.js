14cc3f3b82504e39aa92163e24f004d094978cbd
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-05-16 16:19:43 +0900

    ⚡️ : 화면 이동

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,25 +1,9 @@
 import React from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import Footer from "../../Layout/Footer";
 import Nav from "../../Layout/Nav";
 import "../../styles/HomePage.css";
 
 function HomePage() {
-  const navigate = useNavigate();
-
-  // 로그아웃
-  const onClickHandler = () => {
-    // 복잡하지 x 기능이므로 axios 바로 써줌
-    axios.get("/api/users/logout").then((response) => {
-      if (response.data.success) {
-        navigate("/login");
-      } else {
-        alert("fail logout");
-      }
-    });
-  };
-
   return (
     <div className="home">
       <Nav />
@@ -29,7 +13,6 @@ function HomePage() {
       <div className="map map-dinner"></div>
       <div className="map map-night"></div>
 
-      <button onClick={onClickHandler}>로그아웃</button>
 
       <Footer />
     </div>
