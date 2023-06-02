96396ec684548cd9328d4ab497e79a4419fd2211
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-06-02 09:42:30 +0900

    bg, 캐릭터 화면에 띄우고 캐릭터 움직임(키보드)에 따라 bg의 좌표 이동하기

--- client/src/pages/GamePage/GamePage.js
+++ client/src/pages/GamePage/GamePage.js
@@ -1,20 +1,52 @@
 import { useNavigate } from "react-router-dom";
-import Nav from "../../Layout/Nav";
-import "../../styles/FootPrintPage.css";
+import { useEffect, useState } from "react"
+import "../../styles/GamePage.css";
+import bgImg from '../../gamebg_ocean.png';
 
 function GamePage() {
   const navigate = useNavigate();
+  const [characterPositionX, setCharacterPositionX] = useState(50); // 캐릭터의 초기 x 좌표
+  const [backgroundPositionX, setBackgroundPositionX] = useState(0); // 배경 이미지의 초기 x 좌표
 
+  const handleKeyDown = (event) => {
+    if(event.key == 'ArrowLeft'){
+      setCharacterPositionX(prevX => prevX - 10);
+      setBackgroundPositionX(prevX => prevX + 10);
+    } else if(event.key == 'ArrowRight'){
+      setCharacterPositionX(prevX => prevX + 10);
+      setBackgroundPositionX(prevX => prevX - 10);
+    }
+  };
+
+  useEffect(()=>{
+    window.addEventListener('keydown', handleKeyDown);
+
+    return ()=>{
+      window.addEventListener('keydown', handleKeyDown);
+    };
+  }, []);
   return (
-    <div>
-      <h1>게임페이지</h1>
-      <button
-        onClick={() => {
-          navigate("/");
+    <div className="screen">
+      <div className="bg" 
+            style={{
+              backgroundImage: `url(${bgImg})`,
+              left:`${backgroundPositionX}px`,
+              position: 'absolute',
+              top: '0',
+              height: '1080px',
+              width: '5760px'}}>
+                
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: '0', // 캐릭터의 초기 y 좌표
+          left: `${characterPositionX}px`,
+          height: '50px', // 캐릭터의 높이
+          width: '50px', // 캐릭터의 너비
+          backgroundColor: 'red', // 캐릭터의 색상
         }}
-      >
-        돌아가기
-      </button>
+      />
     </div>
   );
 }
