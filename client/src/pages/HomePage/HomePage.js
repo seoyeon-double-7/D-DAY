aa7119554a8894af793eaf48e3a3329101cea417
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-06-16 12:59:50 +0900

    배경음악 넣기

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,7 +2,9 @@ import React from "react";
 import Footer from "../../Layout/Footer";
 import Nav from "../../Layout/Nav";
 import "../../styles/HomePage.css";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import ReactAudioPlayer from "react-audio-player";
 
 function HomePage() {
   const navigate = useNavigate();
@@ -10,16 +12,19 @@ function HomePage() {
     navigate("/d-day");
   };
 
+
+  useEffect(() => {
+    const audioElement = document.getElementById("backgroundAudio");
+    audioElement.play();
+  }, []);
+
   return (
     <div className="home">
+      <ReactAudioPlayer id="backgroundAudio" src={"/audio/home.mp3" } autoPlay={true} loop style={{display:"none"}}/>
       <Nav />
       <div className="game-maps" onClick={navigateToGame}>
         <img className="map map-morning" src={`/images/morning.png`} alt="" />
-        <img
-          className="map map-afternonn"
-          src={`/images/afternoon.png`}
-          alt=""
-        />
+        <img className="map map-afternonn" src={`/images/afternoon.png`} alt=""/>
         <img className="map map-dinner" src={`/images/diner.png`} alt="" />
         <img className="map map-night" src={`/images/night.png`} alt="" />
       </div>
