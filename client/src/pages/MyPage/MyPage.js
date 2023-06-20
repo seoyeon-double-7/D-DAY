3b573621cfbba664ca294dca54c208bb93cf28c3
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-06-21 08:13:31 +0900

    로그인 한 유저가 쓴 글 마이페이지에 표시

--- client/src/pages/MyPage/MyPage.js
+++ client/src/pages/MyPage/MyPage.js
@@ -1,14 +1,44 @@
 import Nav from "../../Layout/Nav";
 import Footer from "../../Layout/Footer";
 import "../../styles/MyPage.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getPost } from "../../_actions/footprint_action";
 // import profileImg from "../../../public/images/profile_img_sample.png";
 
 function MyPage() {
+  const [Posts, setPosts] = useState({});
+  const [MyPosts, setMyPosts] = useState({});
+  const dispatch = useDispatch();
   // redux에서 사용자 로그인 여부 상태 가져오기
   // 참고 : https://phsun102.tistory.com/85
 
+  // 유저정보
   const user = useSelector((state) => state.user.userData);
+
+  // 방명록 포스트 가져오기
+  const getPosts = () => {
+    dispatch(getPost())
+    .then((res) => {
+      setPosts(res.payload.postData);
+      console.log(res.payload.postData);
+      filterMyPosts(Posts);
+    })
+    
+  };
+  
+  const filterMyPosts = (posts) => {
+    // user.name과 post.name이 같으면 내가 쓴 Post MyPosts에 넣기
+    const filteredPosts = Object.values(posts).filter((post) => post.name === user.name);
+    setMyPosts(filteredPosts);
+  };
+
+  useEffect(() => {
+    getPosts();
+    console.log(Posts);
+    console.log(MyPosts);
+  }, []);
+
   return (
     <div className="mypage-background">
       <Nav />
@@ -21,12 +51,28 @@ function MyPage() {
           alt="sdfd"
         />
         <div className="MypageTextContainer">
-          <span className="profile-name">{user.name}</span>
-          <span className="profile-email">{user.email}</span>
+          <div className="profile-name">{user.name}</div> 
+          <div className="profile-email">{user.email}</div>
         </div>
       </div>
 
-      <div className="footprint-box"></div>
+      <div className="footprint-box">
+      <ul className="posts">
+            {Object.values(MyPosts).map(function (element) {
+              return (
+                <li>
+                  <div className="footprint-post">
+                    <span className="footprint-post-name">{element.name}</span>
+                    <br />
+                    <span className="footprint-post-contents">{element.contents}</span>
+                    <br />
+                    <span className="footprint-post-date">{element.date}</span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+      </div>
 
       <Footer />
     </div>
